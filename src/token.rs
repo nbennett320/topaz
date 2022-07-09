c4eb93c8126d5715c20bc377fee7d5ecc716f943
Sam Kravitz <samkravitz99@gmail.com>
2022-07-09 13:47:12 -0400

    Add token.rs

---
+++ src/token.rs
@@ -0,0 +1,68 @@
+enum TokenType {
+    // 1 character tokens
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+
+    // 1 or 2 character tokens
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+
+    // Literals
+    IDENTIFIER(String),
+    STRING(String),
+    NUMBER(f64),
+
+    // Keywords
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FOR,
+    FN,
+    IF,
+    NIL,
+    OR,
+    PUTS,
+    RETURN,
+    SUPER,
+    SELF,
+    TRUE,
+    VAR,
+    WHILE,
+
+    ERROR,
+    EOF,
+}
+
+struct Token {
+    token_type: TokenType,
+    line: usize,
+    col: usize,
+    len: usize,
+}
+
+impl Token {
+    fn new(token_type: TokenType, line: usize, col: usize, len: usize) -> Token {
+        Token {
+            token_type,
+            line,
+            col,
+            len,
+        }
+    }
+}
