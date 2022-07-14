9e20cf09652ce422ae5f35ad679c21648257fa50
Sam Kravitz <samkravitz99@gmail.com>
2022-07-13 23:31:28 -0400

    Add Value::is_falsey() and tests

--- src/value.rs
+++ src/value.rs
@@ -13,4 +13,37 @@ impl Value {
             Value::Number(num) => print!("{}", num),
         }
     }
+
+    pub fn is_falsey(self) -> bool {
+        match self {
+            Value::Nil => true,
+            Value::Bool(b) => !b,
+            _ => false,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn true_is_not_falsey() {
+        assert_eq!(Value::Bool(true).is_falsey(), false);
+    }
+
+    #[test]
+    fn false_is_falsey() {
+        assert_eq!(Value::Bool(false).is_falsey(), true);
+    }
+
+    #[test]
+    fn nil_is_falsey() {
+        assert_eq!(Value::Nil.is_falsey(), true);
+    }
+
+    #[test]
+    fn numbers_are_not_falsey() {
+        assert_eq!(Value::Number(3.14).is_falsey(), false);
+    }
 }
