7804818fd4f60ef065c0bcb7512889eab1bbe88f
Sam Kravitz <samkravitz99@gmail.com>
2022-07-13 22:44:55 -0400

    Remove TokenType::to_usize

--- src/token.rs
+++ src/token.rs
@@ -74,50 +74,6 @@ impl Token {
 }
 
 impl TokenType {
-    pub fn to_usize(self) -> usize {
-        match self {
-            TokenType::LeftParen => 0,
-            TokenType::RightParen => 1,
-            TokenType::LeftBrace => 2,
-            TokenType::RightBrace => 3,
-            TokenType::Comma => 4,
-            TokenType::Dot => 5,
-            TokenType::Minus => 6,
-            TokenType::Plus => 7,
-            TokenType::Semicolon => 8,
-            TokenType::Slash => 9,
-            TokenType::Star => 10,
-            TokenType::Bang => 11,
-            TokenType::BangEqual => 12,
-            TokenType::Equal => 13,
-            TokenType::EqualEqual => 14,
-            TokenType::Greater => 15,
-            TokenType::GreaterEqual => 16,
-            TokenType::Less => 17,
-            TokenType::LessEqual => 18,
-            TokenType::Identifier(_) => 19,
-            TokenType::String(_) => 20,
-            TokenType::Number(_) => 21,
-            TokenType::And => 22,
-            TokenType::Class => 23,
-            TokenType::Else => 24,
-            TokenType::False => 25,
-            TokenType::For => 26,
-            TokenType::Fn => 27,
-            TokenType::If => 28,
-            TokenType::Nil => 29,
-            TokenType::Or => 30,
-            TokenType::Puts => 31,
-            TokenType::Return => 32,
-            TokenType::Super => 33,
-            TokenType::This => 34,
-            TokenType::True => 35,
-            TokenType::Var => 36,
-            TokenType::While => 37,
-            TokenType::Error(_) => 38,
-        }
-    }
-
     pub fn rule(&self) -> &'static ParseRule {
         match self {
             TokenType::LeftParen => &ParseRule {
